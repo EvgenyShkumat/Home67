2839d08bb345d14e4d93e6b8d6b03ebcb82a90aa
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-04 08:59:32 +0300

    Add project files.

---
+++ main37/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define N 5
+#define M 5
+
+using namespace std;
+
+void random_matrix_init(int matrix[N][M], int min, int max);
+string convert_matrix_to_string(int matrix[N][M]);
+
+int main() {
+
+	return 0;
+}
